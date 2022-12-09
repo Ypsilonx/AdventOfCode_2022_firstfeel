cbb3b5aeb5e2d67fec7130cfc62589f458d61e96
ypsilonx <cibulec.tom@gmail.com>
2022-12-09 17:04:06 +0100

    ...

---
+++ test6.py
@@ -0,0 +1,6 @@
+signal_1 = ['mjqjpqmgbljsphdztnvjfqwrcgsmlb']
+signal_2 = ['bvwbjplbgvbhsrlpgdmjqwftvncz']
+signal_3 = ['nppdvjthqldpwncqszvftbrmjlhg']
+signal_4 = ['nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg']
+signal_5 = ['zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw']
+
